0035df29bc1db03ca52fdce8867bc4e59aa09222
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2024-09-29 20:08:16 +0530

    fron-end ui established

---
+++ frontend/src/_ui_design/components/Sidebar/Conversations.tsx
@@ -0,0 +1,17 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col flex-1 overflow-y-auto ">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+
+export default Conversations;
